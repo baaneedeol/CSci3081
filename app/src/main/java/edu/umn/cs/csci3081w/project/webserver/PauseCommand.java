f7486e04548ffb99c9a01af558c90ca4d98fb6b9
Joey <vohno013@umn.edu>
2024-10-20 21:00:35 -0500

    Added simulation pausing

--- app/src/main/java/edu/umn/cs/csci3081w/project/webserver/PauseCommand.java
+++ app/src/main/java/edu/umn/cs/csci3081w/project/webserver/PauseCommand.java
@@ -1,7 +1,24 @@
-package edu.umn.cs.csci3081w.project.model;
+package edu.umn.cs.csci3081w.project.webserver;
 
-import edu.umn.cs.csci3081w.project.webserver.SimulatorCommand;
+import com.google.gson.JsonObject;
 
 public class PauseCommand extends SimulatorCommand {
+
+  private VisualTransitSimulator simulator;
+
+  public PauseCommand(VisualTransitSimulator simulator) {
+    this.simulator = simulator;
+  }
+
+  /**
+   * Pauses and resumes the simulation.
+   *
+   * @param session current simulation session
+   * @param command the pause simulation command content
+   */
+  @Override
+  public void execute(WebServerSession session, JsonObject command) {
+    simulator.togglePause();
+  }
 
 }
